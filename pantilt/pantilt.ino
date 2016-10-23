29746412ddd0df101964dfec69303ce3466f2f4f
myu0917 <myu0917@kaist.ac.kr>
2016-10-22 21:14:57 -0400

    revised

--- pantilt/pantilt.ino
+++ pantilt/pantilt.ino
@@ -3,13 +3,19 @@
 Servo servo1;
 Servo servo2;
 
-int ang1;
-int ang2;
+float ang1 = 0;
+float ang2 = 0;
 
-int ang1_0;
-int ang1_1;
-int ang2_0;
-int ang2_1;
+float ang1_r = 0;
+float ang2_r = 0;
+
+float ang1_0 = 0;
+float ang1_1 = 0;
+float ang2_0 = 0;
+float ang2_1 = 0;
+
+float delta_ang1;
+float delta_ang2;
 
 void setup() {
   servo1.attach(9);
@@ -17,86 +23,84 @@ void setup() {
 }
 
 void loop() {
+  ang1_r = packet[0];
+  ang2_r = packet[1];
+ 
   ang1_0 = ang1_1;
-  ang1_1 = ang1;
+  ang1_1 = ang1_r;
   ang2_0 = ang2_1;
-  ang2_1 = ang2;
+  ang2_1 = ang2_r;
 
-  if (abs(ang1_1 - ang1_0) <= 1) {
-    break;
-  }
-  else {
-    servo1.write(ang1);
-  }
+  delta_ang1 = ang1_1 - ang1_0;
+  delta_ang2 = ang2_1 - ang2_0;
 
-  if (abs(ang2_1 - ang2_0) <= 1) {
-    break;
-  }
-  else {
-    servo2.write(ang2);
-  }
+  ang1 = ang1 + delta_ang1;
+  ang2 = ang2 + delta_ang2;
+
+  servo1.write(ang1);
+  servo2.write(ang2);
 }
 
 
 
-//================================= motor =================================//
-
-#include <Wire.h>
-#include <Adafruit_MotorShield.h>
-#include "utility/Adafruit_MS_PWMServoDriver.h"
-
-//Stack
-//Creating motor shield object w/ default I2C address
-Adafruit_MotorShield AFMS = Adafruit_MotorShield();
-
-//setting the motor
-Adafruit_DCMotor *motorR = AFMS.getMotor(2); //right motor
-Adafruit_DCMotor *motorL = AFMS.getMotor(1); //left motor
-
-int dist;
-
-void setup() {
-  //setup motor
-  Serial.begin(9600); //setting up Serial library at 9600 bps
-  AFMS.begin(); //create with default frequency 1.6kHz
-  //starting direction (forward, backward, release)
-  motorL->run(BACKWARD);
-  motorR->run(FORWARD);
-}
-
-void goStraight() {
-  motorR->setSpeed(20);
-  motorL->setSpeed(20);
-}
-
-void turnLeft() {
-  motorR->setSpeed(40);
-  motorL->setSpeed(20);
-}
-
-void turnRight() {
-  motorR->setSpeed(20);
-  motorL->setSpeed(40);
-}
-
-void loop() {
-  if (dist <= 10) {             // if there is not detected face(dist = 0? or -1?) or the distance is too close, break
-    break;
-  }
-  else {
-    if (ang1 >= -5 and ang1 <= 5) {
-      goStraight();
-    }
-    else if (ang1 >= 0) {
-      turnRight();
-    }
-    else if (ang1 <= 0) {
-      turnLeft();
-    }
-  }
-  Serial.print(dist);
-  Serial.print("\t");
-  Serial.print(ang1);
-  Serial.print("\t");
-  Serial.println(ang2);
-}
+////================================= motor =================================//
+//
+//#include <Wire.h>
+//#include <Adafruit_MotorShield.h>
+//#include "utility/Adafruit_MS_PWMServoDriver.h"
+//
+////Stack
+////Creating motor shield object w/ default I2C address
+//Adafruit_MotorShield AFMS = Adafruit_MotorShield();
+//
+////setting the motor
+//Adafruit_DCMotor *motorR = AFMS.getMotor(2); //right motor
+//Adafruit_DCMotor *motorL = AFMS.getMotor(1); //left motor
+//
+//int dist;
+//
+//void setup() {
+//  //setup motor
+//  Serial.begin(9600); //setting up Serial library at 9600 bps
+//  AFMS.begin(); //create with default frequency 1.6kHz
+//  //starting direction (forward, backward, release)
+//  motorL->run(BACKWARD);
+//  motorR->run(FORWARD);
+//}
+//
+//void goStraight() {
+//  motorR->setSpeed(20);
+//  motorL->setSpeed(20);
+//}
+//
+//void turnLeft() {
+//  motorR->setSpeed(40);
+//  motorL->setSpeed(20);
+//}
+//
+//void turnRight() {
+//  motorR->setSpeed(20);
+//  motorL->setSpeed(40);
+//}
+//
+//void loop() {
+//  if (dist <= 10) {             // if there is not detected face(dist = 0? or -1?) or the distance is too close, break
+//    break;
+//  }
+//  else {
+//    if (ang1 >= -5 and ang1 <= 5) {
+//      goStraight();
+//    }
+//    else if (ang1 >= 0) {
+//      turnRight();
+//    }
+//    else if (ang1 <= 0) {
+//      turnLeft();
+//    }
+//  }
+//  Serial.print(dist);
+//  Serial.print("\t");
+//  Serial.print(ang1);
+//  Serial.print("\t");
+//  Serial.println(ang2);
+//}
