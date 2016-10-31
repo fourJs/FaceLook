9eb1a5cf6cc6af57615618406044d4ed4ad5abae
myu0917 <myu0917@kaist.ac.kr>
2016-10-31 16:39:41 -0400

    Add pancar

---
+++ pancar/pancar.ino
@@ -0,0 +1,149 @@
+#include <Servo.h>
+#include <Wire.h>
+#include <Adafruit_MotorShield.h>
+#include "utility/Adafruit_MS_PWMServoDriver.h"
+
+//Creating motor shield object w/ default I2C address
+Adafruit_MotorShield AFMS = Adafruit_MotorShield();
+
+//setting the motor
+Adafruit_DCMotor *motorR = AFMS.getMotor(2); //right motor
+Adafruit_DCMotor *motorL = AFMS.getMotor(1); //left motor
+
+Servo servo1;
+
+char incomingByte = 0;
+String packet = "";
+int append = 0;
+
+//float ang1 = 0.0;
+//float ang2 = 0.0;
+float dist = 0.0;
+
+float ang1_r = 0.0;
+float ang2_r = 0.0;
+
+//float ang1_0 = 0.0;
+//float ang1_1 = 0.0;
+//float ang2_0 = 0.0;
+//float ang2_1 = 0.0;
+//float delta_ang1;
+//float delta_ang2;
+
+void setup() {
+  servo1.attach(9);
+  Serial.begin(9600); //setting up Serial library at 9600 bps
+  servo1.write(90);
+  establishContact();
+  AFMS.begin(); //create with default frequency 1.6kHz
+  //starting direction (forward, backward, release)
+  motorL->run(BACKWARD);
+  motorR->run(FORWARD);
+}
+
+void goStraight() {
+  motorR->setSpeed(20);
+  motorL->setSpeed(20);
+}
+
+void turnLeft() {
+  motorR->setSpeed(40);
+  motorL->setSpeed(20);
+}
+
+void turnRight() {
+  motorR->setSpeed(20);
+  motorL->setSpeed(40);
+}
+
+void establishContact() {
+  while (Serial.available() <= 0) {
+    Serial.print('A');   // send a capital A
+    delay(300);
+  }
+}
+
+void parsepacket(String packet1) {
+  int len = packet1.length();
+  packet1.remove(0, 1);
+  packet1[len - 2] = '\0';
+  int cmaIdx = packet1.indexOf(',');
+  int scmaIdx = packet1.indexOf(',', cmaIdx + 1);
+  String sTheta = packet1.substring(0, cmaIdx - 1);
+  String sPhi = packet1.substring(cmaIdx + 2, scmaIdx);
+  String sDist = packet1.substring(scmaIdx + 2);
+  sTheta.trim();
+  sPhi.trim();
+  sDist.trim();
+  Serial.print("before:");
+  Serial.print(sTheta);
+  Serial.print(" ");
+  Serial.print(sPhi);
+  Serial.print(" ");
+  Serial.println(sDist);
+  //    String sThetat= "80.9";
+  //    String sPhit = "91.05";
+  //    String sDistt = "60.1";
+
+  ang1_r = sTheta.toInt();
+  ang2_r = sPhi.toInt();
+  dist = sDist.toInt();
+  Serial.print("after:");
+  Serial.print(ang1_r);
+  Serial.print("\t");
+  Serial.print(ang2_r);
+  Serial.print("\t");
+  Serial.println(dist);
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    // read the incoming byte:
+    incomingByte = Serial.read();
+    if (incomingByte == ';') {
+      parsepacket(packet);
+
+//      ang1_0 = ang1_1;
+//      ang1_1 = ang1_r;
+//      ang2_0 = ang2_1;
+//      ang2_1 = ang2_r;
+//
+//      delta_ang1 = ang1_1 - ang1_0;
+//      delta_ang2 = ang2_1 - ang2_0;
+//
+//      ang1 = ang1 + delta_ang1;
+//      ang2 = ang2 + delta_ang2;
+
+      Serial.println(ang2_r);
+
+      servo1.write(ang2_r);
+
+
+      
+
+//      if (dist <= 10) {             // if there is not detected face(dist = 0) or the distance is too close, break
+//        break;
+//      }
+//      else {
+//        if (ang1 >= -5 and ang1 <= 5) {
+//          goStraight();
+//        }
+//        else if (ang1 >= 0) {
+//          turnRight();
+//        }
+//        else if (ang1 <= 0) {
+//          turnLeft();
+//        }
+      }
+
+      packet = "";
+
+    }
+    else {
+      packet += incomingByte;
+    }
+  }
+  else {
+    establishContact();
+  }
+}
