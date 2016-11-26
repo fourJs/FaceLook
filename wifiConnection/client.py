1043aea8a5b56af8ee2ecb0a414da3fc326c6e17
zhecan wang <zhecanwang@zhecans-MacBook-Pro.local>
2016-11-26 17:17:31 -0500

    update client.py

--- wifiConnection/client.py
+++ wifiConnection/client.py
@@ -8,49 +8,47 @@ import struct
 
 runFlag = True
 
-# Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# Connect the socket to the port where the server is listening
 server_address = ('192.168.32.132', 1234)
 print >>sys.stderr, 'connecting to %s port %s' % server_address
 sock.connect(server_address)
 data = ""
 
-with picamera.PiCamera() as camera:
-    with picamera.array.PiRGBArray(camera) as stream:
-        camera.resolution = (160, 128) 
+# with picamera.PiCamera() as camera:
+#     with picamera.array.PiRGBArray(camera) as stream:
+#         camera.resolution = (160, 128) 
 
-        while (runFlag):
-            camera.capture(stream, 'bgr', use_video_port=True)
-            # stream.array now contains the image data in BGR order
-            frame = stream.array
-            cv2.imshow('frame',frame)
+#         while (runFlag):
+#             camera.capture(stream, 'bgr', use_video_port=True)
+#             # stream.array now contains the image data in BGR order
+#             frame = stream.array
+#             cv2.imshow('frame',frame)
 
             
-            # try:   
-            #     Send data
+#             # try:   
+#             #     Send data
         
-            message = pickle.dumps(frame)
-            sock.sendall(struct.pack("L", len(message))+ message) ### new code
+#             message = pickle.dumps(frame)
+#             sock.sendall(struct.pack("L", len(message))+ message) ### new code
 
 
-            #     print sys.getsizeof(message)
-            #     sock.sendall(message)       
-            #    while data!="I got it":
-            #        data = sock.recv(8) 
-            #        print >>sys.stderr, 'received "%s"' % data
-            # except Exception as e:
-            #     pass
+#             #     print sys.getsizeof(message)
+#             #     sock.sendall(message)       
+#             #    while data!="I got it":
+#             #        data = sock.recv(8) 
+#             #        print >>sys.stderr, 'received "%s"' % data
+#             # except Exception as e:
+#             #     pass
 
-            data = ""
+#             data = ""
             
-            stream.seek(0)
-            stream.truncate()
+#             stream.seek(0)
+#             stream.truncate()
 
-            k = cv2.waitKey(1)
-            if k == ord('q'):
-                runFlag = False
-                cv2.destroyAllWindows()
+#             k = cv2.waitKey(1)
+#             if k == ord('q'):
+#                 runFlag = False
+#                 cv2.destroyAllWindows()
                 
 print >>sys.stderr, 'closing socket'
 sock.close()
