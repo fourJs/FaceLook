239013ce1ea13ce19679c60636244ee3c4f65662
zhecan wang <zhecanwang@zhecans-MacBook-Pro.local>
2016-11-26 16:43:01 -0500

    add cleaned files

--- nanpyTest.py
+++
@@ -1,7 +0,0 @@
-from nanpy import Servo
-import time
-servo = Servo(9)
-for move in [0,97,98,99,100,0]:
-    print move
-    servo.write(move)
-    time.sleep(0.5)
--- wifiConnection/client.py
+++ wifiConnection/client.py
@@ -10,7 +10,7 @@ runFlag = True
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # Connect the socket to the port where the server is listening
-server_address = ('192.168.33.237', 1234)
+server_address = ('192.168.32.132', 1234)
 print >>sys.stderr, 'connecting to %s port %s' % server_address
 sock.connect(server_address)
 data = ""
