962a9671aa6c1bbf203ac800bcae8587b9a50909
zhecan wang <zhecanwang@zhecans-MacBook-Pro.local>
2016-11-30 14:58:48 -0500

    modify server and client connection

--- wifiConnection/simpleServer.py
+++ wifiConnection/simpleServer.py
@@ -9,37 +9,35 @@ import struct ## new
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Bind the socket to the port
-server_address = ('192.168.34.196', 5000)
+server_address = ('192.168.34.189', 5000)
 print >>sys.stderr, 'starting up on %s port %s' % server_address
 sock.bind(server_address)
 
 # Listen for incoming connections
 sock.listen(10)
 
+# Wait for a connection
+print >>sys.stderr, 'waiting for a connection'
+connection, client_address = sock.accept()
 
-### new
 data = ""
-while True:
-    # Wait for a connection
-    print >>sys.stderr, 'waiting for a connection'
-    connection, client_address = sock.accept()
 
-    # try:
+# try:
 
-    print >>sys.stderr, 'connection from', client_address
+print >>sys.stderr, 'connection from', client_address
 
     # Receive the data in small chunks and retransmit it
     while True:
         data = connection.recv(16)
         print >>sys.stderr, 'received "%s"' % data
 
-        if data:
+        # if data:
         #     print >>sys.stderr, 'sending data back to the client'
-            reply = "I got it"
-            connection.sendall(reply)
-        else:
-            print >>sys.stderr, 'no more data from', client_address
-            break
+            # reply = "I got it"
+            # connection.sendall(reply)
+        # else:
+            # print >>sys.stderr, 'no more data from', client_address
+            # break
 
     # except Exception as e:
     #     pass       
