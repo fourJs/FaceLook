b0a07a411278826aeda8e522c251cbaaf14f84fe
zhecan wang <zhecanwang@zhecans-MacBook-Pro.local>
2016-12-07 22:13:32 -0500

    clean files

--- prepareAtt400Dataset.py
+++
@@ -1,8 +0,0 @@
-from sklearn import datasets
-faces = datasets.fetch_olivetti_faces()
-face = faces.images
-import scipy.misc
-
-for i in range(len(face)):
-	f = face[i]
-	scipy.misc.imsave('dataBase/attFaces/' + str(i) + '.jpg', f)
