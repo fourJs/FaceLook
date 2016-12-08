743f483ceaefab4922cdabbbaad1c8d32ad57969
Jong Nam <jwnam0709@gmail.com>
2016-12-08 21:02:35 +0000

    update nanpyTest.py

--- nanpyTest.py
+++ nanpyTest.py
@@ -6,37 +6,21 @@
 from nanpy import Servo
 import time
 
-servo1 = Servo(5)
+servo1 = Servo(6)
 servo2 = Servo(9)
 
-##servo1.write(0)
-##print servo1.read()
-##servo2.write(0)
+servo1.write(20)
+time.sleep(1)
+print "wrote to servo1"
+servo2.write(20)
+print "wrote to servo2"
+time.sleep(3)
 
-##servo2.writeMicroseconds(1430)
-##servo1.writeMicroseconds(1430)
-##print servo1.read()
-##time.sleep(1)
-####
-##servo2.write(20)
-##servo1.write(20)
-
-##servo1.write(0)
-##servo2.write(0)
-
-
-        
-for move in range(544,2400):
-    print move
-    servo2.writeMicroseconds(move)
-    servo1.writeMicroseconds(move)
-    print servo1.read()
-    print servo2.read()
-    time.sleep(0.01)
 
     
 servo1.write(0)
 servo2.write(0)
+print "done"
 ##
 ##del servo1
 ##del servo2
