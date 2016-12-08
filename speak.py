34a8354385d43e3f47b5a5e6fa9f81a001ede022
zhecan wang <zhecanwang@zhecans-MacBook-Pro.local>
2016-12-08 17:05:38 -0500

    test pyttsx

--- speak.py
+++ speak.py
@@ -36,6 +36,6 @@
 
 import pyttsx
 engine = pyttsx.init()
-engine.say('Sally sells seashells by the seashore.')
-engine.say('The quick brown fox jumped over the lazy dog.')
-engine.runAndWait()
+# engine.say('Sally sells seashells by the seashore.')
+# engine.say('The quick brown fox jumped over the lazy dog.')
+# engine.runAndWait()
