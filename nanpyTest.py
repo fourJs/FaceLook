908307e1c504eb72ab604937c4c4e62069325ac8
Jong Nam <jwnam0709@gmail.com>
2016-12-08 22:02:12 +0000

    modify nanpy.py to control wheels

--- nanpyTest.py
+++ nanpyTest.py
@@ -3,25 +3,34 @@
 # Author: Andrea Stagi <stagi.andrea@gmail.com>
 # Description: move a servo motor
 # Dependencies: None
-from nanpy import Servo
+from nanpy import ArduinoApi
+from nanpy import SerialManager
 import time
 
-servo1 = Servo(6)
-servo2 = Servo(9)
+connection = SerialManager(device='/dev/ttyACM0')
 
-servo1.write(20)
-time.sleep(1)
-print "wrote to servo1"
-servo2.write(20)
-print "wrote to servo2"
-time.sleep(3)
+a = ArduinoApi(connection = connection)
+a.pinMode(8, a.OUTPUT)
+a.pinMode(9, a.OUTPUT)
+a.pinMode(10, a.OUTPUT)
+a.pinMode(11, a.OUTPUT)
+a.pinMode(5, a.OUTPUT)
+a.pinMode(6, a.OUTPUT)
 
 
-    
-servo1.write(0)
-servo2.write(0)
-print "done"
-##
-##del servo1
-##del servo2
-    
+a.digitalWrite(8, a.HIGH)
+a.digitalWrite(9, a.LOW)
+
+a.digitalWrite(10, a.HIGH)
+a.digitalWrite(11, a.LOW)
+
+for move in range(100,255,4):
+    a.analogWrite(5, move)
+    time.sleep(0.5)
+    print move
+
+a.analogWrite(5, 0)
+##connection = np.SerialManager(device='/dev/ttyACM1')
+##a = np.ArduinoApi(connection=connection)
+##a.pinMode(13, a.OUTPUT)
+##a.digitalWrite(13, a.HIGH)
