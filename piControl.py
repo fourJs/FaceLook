75cb642ba9321c6f215c2185287b718e88dcd3d7
zhecan wang <zhecanwang@zhecans-MacBook-Pro.local>
2016-12-11 18:29:05 -0500

    test integration

--- piControl.py
+++ piControl.py
@@ -34,7 +34,7 @@ class PiControl(object):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
         # Bind the socket to the port
-        pi_address = ('192.168.32.53', 5005)
+        pi_address = ('192.168.32.53', 5000)
         print >>sys.stderr, 'starting up on %s port %s' % pi_address
         self.sock.bind(pi_address)
 
