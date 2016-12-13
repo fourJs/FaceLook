d26dcacb18a59f22bf16311595e64476ac2bfc5e
zhecan wang <zhecanwang@zhecans-MacBook-Pro.local>
2016-12-13 00:48:41 -0500

    add voice interaction

--- piControl.py
+++ piControl.py
@@ -45,7 +45,7 @@ class PiControl(object):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
         # Bind the socket to the port
-        pi_address = ('192.168.34.110', 5002)
+        pi_address = ('192.168.34.110', 5000)
         print >>sys.stderr, 'starting up on %s port %s' % pi_address
         self.sock.bind(pi_address)
 
